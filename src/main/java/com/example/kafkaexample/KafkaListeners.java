d686c9fe16b2ca14ad9a188d94123eb4a0892518
Abhimanyu Dwivedi <19746098+ABinfinity@users.noreply.github.com>
2024-02-20 18:56:06 +0530

    Initial commit

---
+++ src/main/java/com/example/kafkaexample/KafkaListeners.java
@@ -0,0 +1,18 @@
+package com.example.kafkaexample;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaListeners {
+
+    @KafkaListener(
+            topics = "abhicode",
+            groupId = "groupId"
+    )
+    void listener(String data){
+
+        System.out.println("Listener received: " + data);
+
+    }
+}
